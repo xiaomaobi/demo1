d5683e26f40895a98f67b91ffde727b23c6e5a94
admin <yinlei555@163.com>
2024-03-26 12:42:47 +0800

    初始化提交

---
+++ demo1/src/main/java/com/example/demo1/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class TestController {
+
+    @Value("${server}")
+    private String server;
+
+    @GetMapping("/test")
+    public String test(){
+        return server;
+    }
+
+}
